e67ed84d9419a0727a1dc01b6ce52c2b631fde10
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-22 23:10:58 +0200

    Retrieve user with all needed data from twitter

--- src/twitter/user_retriever.py
+++ src/twitter/user_retriever.py
@@ -8,8 +8,23 @@ class TwitterDevelopersRetriever:
         self.api_token = api_token
 
     def user(self, username: str) -> TwitterUser:
-        _ = requests.get(
+        user_response = requests.get(
             f"https://api.twitter.com/2/users/by/username/{username}",
             headers={"authorization": f"Bearer {self.api_token}"},
         )
-        return TwitterUser(username, followed_by=[], following=[])
+        raw_user = user_response.json()["data"]
+        followers_response = requests.get(
+            f"https://api.twitter.com/2/users/{raw_user['id']}/followers",
+            headers={"authorization": f"Bearer {self.api_token}"},
+        )
+        raw_followers = followers_response.json()["data"]
+        following_response = requests.get(
+            f"https://api.twitter.com/2/users/{raw_user['id']}/followers",
+            headers={"authorization": f"Bearer {self.api_token}"},
+        )
+        raw_following = following_response.json()["data"]
+        return TwitterUser(
+            username,
+            followed_by=[follower["username"] for follower in raw_followers],
+            following=[following["username"] for following in raw_following],
+        )
