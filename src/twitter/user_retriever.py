bf587ba5a0eb346b71a354633db61e16900da15a
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-22 21:39:23 +0200

    Make actual, but unauthorized request to twitter

--- src/twitter/user_retriever.py
+++ src/twitter/user_retriever.py
@@ -1,3 +1,5 @@
+import requests
+
 from src.twitter.user import TwitterUser
 
 
@@ -6,4 +8,9 @@ class TwitterDevelopersRetriever:
         self.api_token = api_token
 
     def user(self, username: str) -> TwitterUser:
+        response = requests.get(
+            "https://api.twitter.com/2/users/by/username/{usesrname}",
+            headers={"authorization": f"Bearer {self.api_token}"},
+        )
+        response.raise_for_status()
         return TwitterUser(username, followed_by=[], following=[])
--- tests/integration/twitter/test_user_retriever.py
+++ tests/integration/twitter/test_user_retriever.py
@@ -1,13 +1,15 @@
 import unittest
 
-from src.twitter.user import TwitterUser
+from requests import HTTPError
+
 from src.twitter.user_retriever import TwitterDevelopersRetriever
 
 
 class TwitterDevelopersRetrieverTestCase(unittest.TestCase):
     def test_retrieves_connections_of_developers(self) -> None:
-        user = TwitterDevelopersRetriever(api_token="test").user("copalco")
-        self.assertEqual(TwitterUser("copalco", followed_by=[], following=[]), user)
+        with self.assertRaises(HTTPError) as exception_info:
+            TwitterDevelopersRetriever(api_token="test").user("copalco")
+        self.assertEqual(exception_info.exception.response.status_code, 401)
 
 
 if __name__ == "__main__":
