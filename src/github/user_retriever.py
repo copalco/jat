7c879b4922054154fd477350762c2e5a204be7af
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-23 22:03:08 +0200

    Return GithubUser instead of dictionary in GithubUserRetriever

--- src/github/user_retriever.py
+++ src/github/user_retriever.py
@@ -1,7 +1,20 @@
 import requests
+from requests.auth import HTTPBasicAuth
+
+from src.github.user import GithubUser
 
 
 class GithubUserRetriever:
-    def user(self, username: str) -> dict[str, str]:
-        response = requests.get("https://api.github.com/users/USERNAME/orgs")
-        return response.json()
+    def __init__(self, api_token: str) -> None:
+        self.token = api_token
+
+    def user(self, username: str) -> GithubUser:
+        _ = requests.get(
+            f"https://api.github.com/users/{username}/orgs",
+            auth=HTTPBasicAuth("copalco", self.token),
+            headers={
+                "Accept": "application/vnd.github.v3+json",
+                "User-Agent": "copalco",
+            },
+        )
+        return GithubUser(username)
--- tests/integration/github/test_user_retriever.py
+++ tests/integration/github/test_user_retriever.py
@@ -1,14 +1,17 @@
+import os
 import unittest
 
+from src.github.user import GithubUser
 from src.github.user_retriever import GithubUserRetriever
 
 
 class GithubUserRetrieverTestCase(unittest.TestCase):
     def test_retrieves_user_github_user(self):
+        user = GithubUserRetriever(os.environ["JAT_GITHUB_API_TOKEN"]).user(
+            username="defunkt"
+        )
         self.assertEqual(
-            {
-                "documentation_url": "https://docs.github.com/rest/reference/orgs#list-organizations-for-a-user",
-                "message": "Not Found",
-            },
-            GithubUserRetriever().user(username="copalco"),
+            GithubUser("defunkt"),
+            user,
+            user,
         )
