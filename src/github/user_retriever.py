b5debbc5d4f9d2fb8273c7dbad1142fce7d6c451
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-23 20:47:27 +0200

    Make yet failing request to github

--- src/github/user_retriever.py
+++ src/github/user_retriever.py
@@ -1,6 +1,7 @@
-from src.github.user import GithubUser
+import requests
 
 
 class GithubUserRetriever:
-    def user(self, username: str) -> GithubUser:
-        return GithubUser(username)
+    def user(self, username: str) -> dict[str, str]:
+        response = requests.get("https://api.github.com/users/USERNAME/orgs")
+        return response.json()
--- tests/integration/github/test_user_retriever.py
+++ tests/integration/github/test_user_retriever.py
@@ -1,9 +1,14 @@
 import unittest
 
-from src.github.user import GithubUser
 from src.github.user_retriever import GithubUserRetriever
 
 
 class GithubUserRetrieverTestCase(unittest.TestCase):
     def test_retrieves_user_github_user(self):
-        self.assertEqual(GithubUser(username="copalco"), GithubUserRetriever().user(username="copalco"))
+        self.assertEqual(
+            {
+                "documentation_url": "https://docs.github.com/rest/reference/orgs#list-organizations-for-a-user",
+                "message": "Not Found",
+            },
+            GithubUserRetriever().user(username="copalco"),
+        )
