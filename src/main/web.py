ad9370570edfdc1456e2cdbc40bc5d564a646b9b
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-26 08:58:21 +0200

    Add stub endpoint for realtime connection check

--- src/main/web.py
+++ src/main/web.py
@@ -1,6 +1,14 @@
-from falcon import API
+import json
+
+import falcon
 
 
-def create_app() -> API:
-    web_api = API()
+class StubResource:
+    def on_get(self, request: falcon.Request, response: falcon.Response, first_developer_handle: str, second_developer_handle: str) -> None:
+        response.media = {"connected": True}
+
+
+def create_app() -> falcon.API:
+    web_api = falcon.API()
+    web_api.add_route("/connected/realtime/{first_developer_handle}/{second_developer_handle}", StubResource())
     return web_api
