693ba44ef1bdaaafba47f8aae7e03b0a0d6527ab
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-29 19:38:18 +0200

    Add handle value object

---
+++ src/domain/model/handle.py
@@ -0,0 +1,18 @@
+class Handle:
+    def __init__(self, value: str) -> None:
+        if len(value) > 15:
+            raise ValueError("Handle can have up to 15 characters")
+        if len(value) < 4:
+            raise ValueError("Handle must have at least 4 characters")
+        self.value = value
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Handle):
+            return NotImplemented
+        return self.value == other.value
+
+    def __str__(self) -> "str":
+        return self.value
+
+    def __repr__(self) -> "str":
+        return f"{self.__class__.__name__}(value={self.value})"
---
+++ tests/unit/domain/model/test_handle.py
@@ -0,0 +1,31 @@
+import unittest
+
+from src.domain.model.handle import Handle
+
+
+class HandleTestCase(unittest.TestCase):
+    def test_handles_are_equal(self) -> None:
+        self.assertEqual(Handle("test"), Handle("test"))
+
+    def test_handle_to_string_is_its_value(self) -> None:
+        self.assertEqual(str(Handle("test")), "test")
+
+    def test_handle_can_only_be_compared_with_other_handle(self) -> None:
+        class SomethingElse:
+            value = "test"
+
+        self.assertNotEqual(Handle("test"), SomethingElse())
+
+    def test_handle_cannot_be_longer_than_15_characters(self) -> None:
+        with self.assertRaises(ValueError) as exception_info:
+            _ = Handle("test1" * 3 + "1")
+        self.assertEqual(
+            str(exception_info.exception), "Handle can have up to 15 characters"
+        )
+
+    def test_handle_cannot_be_shorter_than_4_characters(self) -> None:
+        with self.assertRaises(ValueError) as exception_info:
+            _ = Handle("tes")
+        self.assertEqual(
+            str(exception_info.exception), "Handle must have at least 4 characters"
+        )
