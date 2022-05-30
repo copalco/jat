bf185d5533e7e3e380ff2bd0c27c9b57eaa87299
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-30 22:53:44 +0200

    Handle not found error

--- src/web/resources/connected.py
+++ src/web/resources/connected.py
@@ -3,6 +3,7 @@ from starlette.responses import JSONResponse, Response
 
 from src.app.are_developers_connected_query import AreDevelopersConnectedQuery
 from src.app.connected_query_handler import ConnectedQueryHandler
+from src.app.errors import Errors
 from src.web.resource import Resource
 
 
@@ -16,14 +17,24 @@ class ConnectedResource(Resource):
         self.query_handler = query_handler
 
     def on_get(self, request: Request) -> Response:
-        relation = self.query_handler.handle(
-            AreDevelopersConnectedQuery(
-                request.path_params["first_developer_handle"],
-                request.path_params["second_developer_handle"],
+        try:
+            relation = self.query_handler.handle(
+                AreDevelopersConnectedQuery(
+                    request.path_params["first_developer_handle"],
+                    request.path_params["second_developer_handle"],
+                )
             )
-        )
-        if relation.connected():
-            return JSONResponse(
-                {"connected": True, "organizations": relation.organizations()}
-            )
-        return JSONResponse({"connected": False})
+        except Errors as e:
+            errors = []
+            for error in e.args[0]:
+                for service in error.absent_on:
+                    errors.append(f"{error.handle} is no valid user in {service}")
+            return JSONResponse({
+                "errors": sorted(errors)
+            })
+        else:
+            if relation.connected():
+                return JSONResponse(
+                    {"connected": True, "organizations": relation.organizations()}
+                )
+            return JSONResponse({"connected": False})
--- tests/unit/web/test_connected_resource.py
+++ tests/unit/web/test_connected_resource.py
@@ -10,14 +10,20 @@ from src.app.developers_relation import (
     DevelopersNotConnected,
     DevelopersRelation,
 )
+from src.app.errors import Errors
+from src.domain.model.developer_not_found import DeveloperNotFound
+from src.domain.model.handle import Handle
 from src.web.resources.connected import ConnectedResource
 
 
 class FakeQueryHandler(ConnectedQueryHandler):
     def __init__(self) -> None:
         self._connected = ["dev1", "dev2"]
+        self._error: Exception | None = None
 
     def handle(self, query: AreDevelopersConnectedQuery) -> DevelopersRelation:
+        if self._error:
+            raise self._error
         if (
             query.first_developer in self._connected
             and query.second_developer in self._connected
@@ -62,5 +68,34 @@ class ConnectedResourceTestCase(unittest.TestCase):
         )
         self.assertEqual(
             JSONResponse({"connected": True, "organizations": ["org1", "org2"]}).body,
+            result.body,
+        )
+
+    def test_returns_errors_list_on_errors(self) -> None:
+        self.query_handler.fail_with(
+            Errors(
+                [DeveloperNotFound(Handle("dev1"), absent_on=["twitter", "github"]), DeveloperNotFound(Handle("dev2"), absent_on=["twitter"])]
+            )
+        )
+        result = self.resource.on_get(
+            Request(
+                scope={
+                    "type": "http",
+                    "path_params": {
+                        "first_developer_handle": "dev1",
+                        "second_developer_handle": "dev2",
+                    },
+                }
+            )
+        )
+        self.assertEqual(
+            JSONResponse({
+                "errors": [
+                    "dev1 is no valid user in github",
+                    "dev1 is no valid user in twitter",
+                    "dev2 is no valid user in twitter",
+                ]
+            }
+            ).body,
             result.body,
         )
