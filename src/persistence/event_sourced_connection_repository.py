41a7aee90992232b15d65c07d8cf750e21e9b75f
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-30 14:34:33 +0200

    Store Connection with event sourced repository

--- src/persistence/event_sourced_connection_repository.py
+++ src/persistence/event_sourced_connection_repository.py
@@ -1,4 +1,16 @@
-class EventSourcedConnectionRepository:
+from src.domain.events.store import EventStore
+from src.domain.events.stream import EventStream
+from src.domain.events.stream_id import EventStreamId
+from src.domain.model.connection import Connection
 
-    def __init__(self, event_store) -> None:
+
+class EventSourcedConnectionRepository:
+    def __init__(self, event_store: EventStore) -> None:
         self._event_store = event_store
+
+    def save(self, connection: Connection) -> None:
+        stream = EventStream(
+            EventStreamId.from_handles(*connection.handles),
+            events=connection.changes(),
+        )
+        self._event_store.store(stream)
--- tests/unit/persistence/test_event_sourced_connection_repository.py
+++ tests/unit/persistence/test_event_sourced_connection_repository.py
@@ -1,22 +1,64 @@
+import datetime
 import unittest
 
+import freezegun
+
+from src.domain.events.events import DevelopersAreNotConnected, Event
 from src.domain.events.store import EventStore
 from src.domain.events.stream import EventStream
 from src.domain.events.stream_id import EventStreamId
-from src.persistence.event_sourced_connection_repository import \
-    EventSourcedConnectionRepository
+from src.domain.model.connection import Connection
+from src.domain.model.developer import Developer
+from src.domain.model.handle import Handle
+from src.persistence.event_sourced_connection_repository import (
+    EventSourcedConnectionRepository,
+)
 
 
 class SpyEventStore(EventStore):
+    def __init__(self) -> None:
+        self._events: dict[EventStreamId, list[Event]] = {}
+
     def store(self, stream: EventStream) -> None:
-        pass
+        self._events[stream.id] = stream.events
 
     def withdraw(self, stream_id: EventStreamId) -> EventStream:
-        pass
+        return EventStream(stream_id, self._events[stream_id])
 
 
-class EventSourcedRedemptionRepository(unittest.TestCase):
-
+class EventSourcedRedemptionRepositoryTestCase(unittest.TestCase):
     def test_stores_object_changes_using_event_store(self) -> None:
+        first_handle = Handle("dev1")
+        second_handle = Handle("dev2")
         event_store: EventStore = SpyEventStore()
-        _ = EventSourcedConnectionRepository(event_store)
+        repository = EventSourcedConnectionRepository(event_store)
+        with freezegun.freeze_time("2022-05-30"):
+            connection = Connection.register(
+                Developer(
+                    first_handle,
+                    follows=[second_handle, Handle("dev5")],
+                    followed_by=[Handle("dev9"), Handle("dev3")],
+                    organizations=["org1", "org2", "org3"],
+                ),
+                Developer(
+                    second_handle,
+                    follows=[Handle("dev3"), Handle("dev5")],
+                    followed_by=[second_handle, Handle("dev3")],
+                    organizations=["org1", "org2", "org3"],
+                ),
+            )
+        repository.save(connection)
+        self.assertEqual(
+            EventStream(
+                EventStreamId("dev1-dev2"),
+                events=[
+                    DevelopersAreNotConnected(
+                        handles=(first_handle, second_handle),
+                        registered_at=datetime.datetime(2022, 5, 30),
+                    ),
+                ],
+            ),
+            event_store.withdraw(
+                EventStreamId.from_handles(first_handle, second_handle)
+            ),
+        )
