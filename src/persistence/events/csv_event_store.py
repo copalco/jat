510a06c627ed7595998dd178c3dbc7280e3a7d8b
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-05-30 19:55:25 +0200

    Set CSV delimiters to ';'

--- src/persistence/events/csv_event_store.py
+++ src/persistence/events/csv_event_store.py
@@ -12,14 +12,14 @@ class CSVEventStore(EventStore):
 
     def store(self, stream: EventStream) -> None:
         with open(self.filepath, "w") as events_file:
-            writer = csv.writer(events_file)
+            writer = csv.writer(events_file, delimiter=";")
             for event in stream.events:
                 writer.writerow([stream.id, *event.to_string()])
 
     def withdraw(self, stream_id: EventStreamId) -> EventStream:
         events: list[Event] = []
         with open(self.filepath) as events_file:
-            reader = csv.reader(events_file)
+            reader = csv.reader(events_file, delimiter=";")
             for row in reader:
                 if EventStreamId(row[0]) == stream_id:
                     events.append(Event.from_string(row))
