25a5118abebaef3f7cfcfe2c066f2ec8680b4ec3
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-06-01 23:24:35 +0200

    Add restore abstract method to connection repository

--- src/domain/model/connection_repository.py
+++ src/domain/model/connection_repository.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 
 from src.domain.model.connection import Connection
+from src.domain.model.connection_id import ConnectionId
 
 
 class ConnectionRepository(ABC):
     @abstractmethod
     def save(self, connection: Connection) -> None:
-        pass
+        raise NotImplementedError()
+
+    @abstractmethod
+    def restore(self, id: ConnectionId) -> Connection:
+        raise NotImplementedError()
